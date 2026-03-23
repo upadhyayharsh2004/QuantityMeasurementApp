4ca5de368979cb0a0cef8f6c194c7fbdfae06223
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-03-23 10:56:18 +0530

    Removed remaining feature-UC16 folder

--- QuantityMeasurementApp-feature-UC16-DatabaseIntegrationADODOTNET/QuantityMeasurementModelLayer/Enums/TemperatureUnit.cs
+++
@@ -1,8 +0,0 @@
-namespace QuantityMeasurementModelLayer.Enums;
-    public enum TemperatureUnit
-    {
-        CELSIUS,
-        FAHRENHEIT
-    }
-
-   
--- QuantityMeasurementApp-feature-UC16-DatabaseIntegrationADODOTNET/QuantityMeasurementModelLayer/Enums/VolumeUnit.cs
+++
@@ -1,9 +0,0 @@
-namespace QuantityMeasurementModelLayer.Enums;
-    public enum VolumeUnit
-    {
-        LITRE,
-        MILLILITRE,
-        GALLON
-    }
-
-    
